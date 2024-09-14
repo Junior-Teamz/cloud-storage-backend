9b8ca6aa2496c195a7e9adf18eef0c99c7f3aaa2
Isjustmy <ifpersona205@gmail.com>
2024-09-15 06:27:42 +0700

    ...

--- app/Http/Middleware/Custom/FileImageURLPermissionCheck.php
+++ app/Http/Middleware/Custom/FileImageURLPermissionCheck.php
@@ -25,7 +25,7 @@ class FileImageURLPermissionCheck
     public function handle($request, Closure $next)
     {
         // Ambil pengguna yang sedang login
-        $user = Auth::user();
+        $user = auth()->guard('api')->user();
 
         // Jika pengguna belum login, tolak akses
         if (!$user) {
