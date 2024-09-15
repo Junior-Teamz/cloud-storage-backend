8d7a797baf4392b6365794bfac5df071c1d110bb
Isjustmy <ifpersona205@gmail.com>
2024-09-16 00:06:15 +0700

    ...

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -418,8 +418,8 @@ class FileController extends Controller
                 // Mendapatkan waktu saat ini dengan zona waktu Jakarta
                 $now = Carbon::now('Asia/Jakarta');
 
-                // Membuat format nama file dengan format d-m-Y_H:i:s
-                $zipFileName = 'files_' . $now->format('d-m-Y_H:i:s') . '.zip';
+                // Membuat format nama file dengan nama dan tanggal waktu
+                $zipFileName = 'files ' . $now->format('d-m-Y_H:i:s') . '.zip';
                 $zipFilePath = storage_path('app/temp/' . $zipFileName);
 
                 // Create ZipArchive
