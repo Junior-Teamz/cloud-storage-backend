624a500e32eeac11a4ba24321f6e257cc4d4e854
Isjustmy <ifpersona205@gmail.com>
2024-09-25 17:36:32 +0700

    ...

--- app/Http/Middleware/Custom/HideSuperadminFlag.php
+++ app/Http/Middleware/Custom/HideSuperadminFlag.php
@@ -23,15 +23,46 @@ class HideSuperadminFlag
         if ($response instanceof \Illuminate\Http\JsonResponse) {
             $data = $response->getData(true);
 
-            // Check if the is_superadmin key exists and remove it
-            if (isset($data['data']['is_superadmin'])) {
-                unset($data['data']['is_superadmin']);
-            }
+            // Recursively remove 'is_superadmin' from the response data
+            $data = $this->removeSuperadminFlag($data);
 
             // Set the modified data back into the response
             $response->setData($data);
         }
 
         return $response;
+    }
+
+    /**
+     * Recursively remove 'is_superadmin' key from an array or object.
+     *
+     * @param  array|object  $data
+     * @return array|object
+     */
+    protected function removeSuperadminFlag($data)
+    {
+        if (is_array($data)) {
+            foreach ($data as $key => $value) {
+                // If 'is_superadmin' key is found, remove it
+                if ($key === 'is_superadmin') {
+                    unset($data[$key]);
+                } else {
+                    // Recursively process nested arrays and objects
+                    $data[$key] = $this->removeSuperadminFlag($value);
+                }
+            }
+        } elseif (is_object($data)) {
+            foreach ($data as $key => $value) {
+                // If 'is_superadmin' property is found, unset it
+                if ($key === 'is_superadmin') {
+                    unset($data->$key);
+                } else {
+                    // Recursively process nested arrays and objects
+                    $data->$key = $this->removeSuperadminFlag($value);
+                }
+            }
+        }
+
+        return $data;
     }
 }
