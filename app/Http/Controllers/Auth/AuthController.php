653bc5c95a96c79ae741b4728b900a570f2501e6
Isjustmy <ifpersona205@gmail.com>
2024-10-18 01:06:06 +0700

    ...

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -61,10 +61,11 @@ class AuthController extends Controller
             // Buat access token ID
             $accessTokenId = JWTAuth::setToken($accessToken)->getPayload()['jti']; // Ambil ID dari access token
 
+            JWTAuth::factory()->setTTL(7 * 24 * 60); // Mengatur TTL untuk refresh token
             $refreshToken = JWTAuth::claims([
                 'access_token_id' => $accessTokenId, // Simpan access token ID di refresh token
                 'additional_time' => 3600 // Waktu tambahan (1 jam)
-            ])->setTTL(7 * 24 * 60)->tokenById($user->id); // Buat refresh token tanpa autentikasi ulang
+            ])->fromUser($user); // Buat refresh token tanpa autentikasi ulang
 
             // Inisialisasi array respons
             $responseData = [
