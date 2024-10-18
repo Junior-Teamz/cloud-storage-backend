b7f46700065596e948bce457de3ae813208d508f
Isjustmy <ifpersona205@gmail.com>
2024-10-18 13:15:46 +0700

    ...

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -892,36 +892,6 @@ class FileController extends Controller
 
     public function serveFileByHashedId($fileId)
     {
-        $user = Auth::user();
-
-        if ($user) {
-
-            // Cari file berdasarkan ID
-            $file = File::find($fileId);
-
-            if (!$file) {
-                return response()->json(['errors' => 'File not found'], 404);  // File tidak ditemukan
-            }
-
-            // Periksa perizinan menggunakan fungsi checkPermissionFile
-            if (!$this->checkPermissionFileServices->checkPermissionFile($file->id, ['read'])) {
-                return response()->json(['errors' => 'You do not have permission to access this URL.'], 403);
-            }
-
-            // Ambil path file dari storage
-            $file_path = Storage::path($file->path);
-
-            // Kembalikan file sebagai respon (mengirim file gambar)
-            return response()->file($file_path);
-        } else {
-            return response()->json([
-                'errors' => 'Unauthenticated.'
-            ], 401);
-        }
-    }
-
-    public function serveFileVideoByHashedId($fileId)
-    {
         // Cari file berdasarkan ID
         $file = File::find($fileId);
 
@@ -929,27 +899,51 @@ class FileController extends Controller
             return response()->json(['errors' => 'File not found'], 404);  // File tidak ditemukan
         }
 
-        // Cek apakah file adalah video
-        if (!Str::startsWith(Storage::mimeType($file->path), 'video')) {
-            return response()->json(['errors' => 'The file is not a video'], 415);  // 415 Unsupported Media Type
-        }
+        // Ambil path file dari storage
+        $file_path = Storage::path($file->path);
 
-        // // Periksa perizinan
-        // if (!$this->checkPermissionFileServices->checkPermissionFile($file->id, ['read'])) {
-        //     return response()->json(['errors' => 'You do not have permission to access this URL.'], 403);
+        // Kembalikan file sebagai respon (mengirim file gambar)
+        return response()->file($file_path);
+    }
+
+    public function serveFileVideoByHashedId($fileId)
+    {
+        // // Cari file berdasarkan ID
+        // $file = File::find($fileId);
+
+        // if (!$file) {
+        //     return response()->json(['errors' => 'File not found'], 404);  // File tidak ditemukan
         // }
+
+        // // Cek apakah file adalah video
+        // if (!Str::startsWith(Storage::mimeType($file->path), 'video')) {
+        //     return response()->json(['errors' => 'The file is not a video'], 415);  // 415 Unsupported Media Type
+        // }
+
+        // // Ambil path file dari storage dan stream
+        // $file_path = Storage::path($file->path);
+
+        // return response()->stream(function () use ($file_path) {
+        //     $stream = fopen($file_path, 'rb');
+        //     fpassthru($stream);
+        //     fclose($stream);
+        // }, 200, [
+        //     'Content-Type' => Storage::mimeType($file->path),
+        //     'Content-Length' => Storage::size($file->path),
+        // ]);
+
+        // Cari file berdasarkan ID
+        $file = File::find($fileId);
+
+        if (!$file) {
+            return response()->json(['errors' => 'File not found'], 404);  // File tidak ditemukan
+        }
 
         // Ambil path file dari storage dan stream
         $file_path = Storage::path($file->path);
 
-        return response()->stream(function () use ($file_path) {
-            $stream = fopen($file_path, 'rb');
-            fpassthru($stream);
-            fclose($stream);
-        }, 200, [
-            'Content-Type' => Storage::mimeType($file->path),
-            'Content-Length' => Storage::size($file->path),
-        ]);
+        $stream = new \App\Services\StreamVideoService($file_path);
+        $stream->start(); 
     }
 
     public function generateFilePublicPath($folderId, $fileName)
