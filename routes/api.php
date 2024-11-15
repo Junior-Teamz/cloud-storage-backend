7c286aa274ba7b2cf8ba111329b99ef39e664e8a
Isjustmy <ifpersona205@gmail.com>
2024-11-15 10:51:21 +0700

    revert back update

--- routes/api.php
+++ routes/api.php
@@ -85,6 +85,8 @@ Route::middleware(['auth:api', 'protectRootFolder', 'protectRootTag', 'check_adm
         Route::get('/index', [UserController::class, 'index']);
 
         Route::put('/update', [UserController::class, 'update']);
+        
+        Route::put('/update_password', [UserController::class, 'updatePassword']);
 
         Route::delete('/delete', [UserController::class, 'delete']);
 
@@ -221,6 +223,8 @@ Route::prefix('admin')->middleware(['auth:api', 'validate_admin'])->group(functi
         Route::post('/create_user', [AdminController::class, 'createUserFromAdmin']); // route untuk membuat user baru melalui admin.
 
         Route::put('/update_user/{userIdToBeUpdated}', [AdminController::class, 'updateUserFromAdmin']); // route untuk mengupdate user yang sudah ada melalui admin.
+        
+        Route::put('/update_user_password/{userId}', [AdminController::class, 'updateUserPassword']); // route untuk mengupdate user yang sudah ada melalui admin.
 
         Route::delete('/delete_user/{userIdToBeDeleted}', [AdminController::class, 'deleteUserFromAdmin']); // route untuk menghapus user yang sudah ada melalui admin. (DANGEROUS!)
     });
