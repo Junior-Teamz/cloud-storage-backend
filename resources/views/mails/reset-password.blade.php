0ef369341f7c481b337dde60c051763384cd4807
Isjustmy <ifpersona205@gmail.com>
2024-11-26 12:46:14 +0700

    change mail view

--- resources/views/mails/reset-password.blade.php
+++ resources/views/mails/reset-password.blade.php
@@ -2,19 +2,18 @@
 <html>
 <head>
     <title>Reset Password</title>
-    
 </head>
 <body>
     <!-- Banner -->
-    <div style="text-align: center; margin-bottom: 20px;">
-        <img src="{{ asset('KemenkopUKM File Sharing Logo.png') }}" alt="KemenkopUKM Logo" style="max-width: 100%; height: auto;">
-    </div>
+    <!-- <div style="text-align: center; margin-bottom: 20px;">
+        <img src="" alt="KemenkopUKM Logo" style="max-width: 100%; height: auto;">
+    </div> -->
 
     <!-- Konten Email -->
-    <h3>Halo, Udin Mauludin!</h3>
+    <h3>Halo, {{ $name }}!</h3>
     <p>Anda menerima email ini karena ada permintaan untuk mereset password akun Anda.</p>
     <p>Klik tautan di bawah ini untuk mengatur ulang password Anda:</p>
-    <a href="{{ route('email_template') }}">(Link password disini)</a>
+    <a href="{{ $resetLink }}">{{ $resetLink }}</a>
     <p>Jika Anda tidak meminta reset password, abaikan email ini.</p>
     <p>Terima kasih</p>
 </body>
