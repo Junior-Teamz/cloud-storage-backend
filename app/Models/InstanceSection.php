8fa7b864b8fceb048140d44376f24913d98c2a1c
Isjustmy <ifpersona205@gmail.com>
2025-01-16 10:04:24 +0700

    fix bug: instance section ID is not UUID

--- app/Models/InstanceSection.php
+++ app/Models/InstanceSection.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,7 +12,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class InstanceSection extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $fillable = [
         'instance_id',
