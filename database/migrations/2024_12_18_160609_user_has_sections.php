ffaf385b23f650842c85b04b316408964849e7de
Isjustmy <ifpersona205@gmail.com>
2025-01-16 10:51:07 +0700

    fix bug: user instance section id error

--- database/migrations/2024_12_18_160609_user_has_sections.php
+++ database/migrations/2024_12_18_160609_user_has_sections.php
@@ -15,10 +15,10 @@ return new class extends Migration
         Schema::create('user_has_sections', function (Blueprint $table) {
             $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
             $table->foreignUuid('user_id')->references('id')->on('users')->cascadeOnDelete();
-            $table->foreignUuid('section_id')->references('id')->on('instance_sections')->cascadeOnDelete();
+            $table->foreignUuid('instance_section_id')->references('id')->on('instance_sections')->cascadeOnDelete();
             $table->timestamps();
 
-            $table->index(['user_id', 'section_id']);
+            $table->index(['user_id', 'instance_section_id']);
         });
     }
 
